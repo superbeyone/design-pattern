c2671cd64d96fdf00ff062bf5a5dad6bf732dcea
superbeyone <superbeyone@outlook.com>
2019-03-18 16:31:24 +0800

    interface gregation

---
+++ src/main/java/com/superbeyone/design/interfacesegregation/EatAnimalAction.java
@@ -0,0 +1,13 @@
+package com.superbeyone.design.interfacesegregation;
+
+/**
+ * @author Mr.superbeyone
+ * @project design-pattern
+ * @className EatAnimalAction
+ * @description
+ * @date 2019-03-18 16:28
+ **/
+
+public interface EatAnimalAction {
+    void eat();
+}
---
+++ src/main/java/com/superbeyone/design/interfacesegregation/FlyAnimalAction.java
@@ -0,0 +1,14 @@
+package com.superbeyone.design.interfacesegregation;
+
+/**
+ * @author Mr.superbeyone
+ * @project design-pattern
+ * @className FlyAnimalAction
+ * @description
+ * @date 2019-03-18 16:27
+ **/
+
+public interface FlyAnimalAction {
+    void fly();
+
+}
