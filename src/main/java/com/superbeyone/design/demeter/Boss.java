dd889467d44ebee4209fafcc824debed8444c5ca
superbeyone <superbeyone@outlook.com>
2019-03-18 17:07:58 +0800

    demeter

---
+++ src/main/java/com/superbeyone/design/demeter/Boss.java
@@ -0,0 +1,16 @@
+package com.superbeyone.design.demeter;
+
+/**
+ * @author Mr.superbeyone
+ * @project design-pattern
+ * @className Boss
+ * @description
+ * @date 2019-03-18 16:37
+ **/
+
+public class Boss {
+    public void countCheck(TeamLeader teamLeader) {
+
+        teamLeader.checkNumOfCourses();
+    }
+}
---
+++ src/main/java/com/superbeyone/design/demeter/Course.java
@@ -0,0 +1,13 @@
+package com.superbeyone.design.demeter;
+
+/**
+ * @author Mr.superbeyone
+ * @project design-pattern
+ * @className Course
+ * @description
+ * @date 2019-03-18 16:38
+ **/
+
+public class Course {
+
+}
